ed10a88c41c633023a53f4b27c9010efa3714334
thiagomarti <thiagomarti2008@gmail.com>
2024-11-12 23:20:00 -0300

    sizing changes

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
     <div class="hero-content">
       <h1>¿Queres saltar al siguiente nivel?</h1>
       <p>Los mejores servicios en Software y Hardware, a la distancia de un click.</p>
-      <a href="#contacto" class="cta-button">
+      <a href="https://wa.me/541123741777?text=Hola%20estoy%20interesado%20en%20uno%20de%20sus%20servicios!" class="cta-button" target="_blank">
         <span class="button__icon-wrapper">
           <svg width="10" class="button__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
             <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
